8df02cbc3bcebe9d4fd77ebe17c7152b0837bbb2
Cody Baker <cody@fluttrhq.com>
2012-09-14 04:48:37 -0700

    sidebar fixed more

--- js/application.js
+++ js/application.js
@@ -7,38 +7,67 @@ function initCufon(){Cufon.replace(".main-container h2, .content-container h2,.b
 
 
 $(document).ready(function() {
-
-	
-	
 	function pin_sidebar() {
-		var sidebar_height = $('.sidebar-content').height();
-		var window_height = $(window).height();
-		var whatever_scrollbar = window.innerHeight;
-		var offset = $(window).scrollTop();
-		var bottom_of_sidebar = sidebar_height + offset;
-		var bottom_of_container = $(".docs").offset().top + $(".docs").height();
-		var sticking_point = bottom_of_container + 10;		
+		sidebar_height = $('.sidebar-content').height();
+		window_height = $(window).height();
+		whatever_scrollbar = window.innerHeight;
+		offset = $(window).scrollTop();
+		bottom_of_sidebar = sidebar_height + offset;
+		bottom_of_container = $(".docs").offset().top + $(".docs").height();
+		sticking_point = bottom_of_container - 20;		
 		
+
+		if ($(window).height() < 484)
+		{
+		    scrolly = $('.sidebar-content').hasClass('scrolly');
+		    if (offset < 200)
+		    {
+		        $('.sidebar-content').attr('style','');
+		        $(".sidebar-content").removeClass("pinned");
+		        $(".sidebar-content").addClass("stuck");
+		        $('.sidebar-content').removeClass('scrolly');
+                return;
+		    }
+		    else if (offset > $('.content-container').height()-$('.content-container').offset().top) //bottom_of_container - 275)
+		    {
+		        $('.sidebar-content').attr('style','');
+		        $('.sidebar-content').css('top', bottom_of_container - 700); 
+		        $(".sidebar-content").removeClass("pinned");
+                	$(".sidebar-content").addClass("stuck");
+                	$('.sidebar-content').removeClass('scrolly');
+        	        return;
+		    }
+		    else if (!scrolly)
+		    {
+		        $('.sidebar-content').attr('style','');
+                	$('.sidebar-content').css('max-height', $(window).height());
+		        $('.sidebar-content').css('overflow-y', "scroll");
+		        $(".sidebar-content").removeClass("stuck");
+                	$(".sidebar-content").addClass("pinned");
+                	$('.sidebar-content').addClass('scrolly');
+                return;
+		    } else if (scrolly){
+		        return;
+		    }
+		}
 		
 		if($(".docs").height() - sidebar_height < 100) {
 			$(".sidebar-content").removeClass("pinned");
 			$(".sidebar-content").removeClass("stuck");
 			$(".sidebar-content").css("top", "");
-			return
+			return;
 		}
-		
 		
 		
 		if (window_height - sidebar_height < 0) {
-			//$('.sidebar-content').css("max-height", whatever_scrollbar);
+			$('.sidebar-content').css("max-height", whatever_scrollbar);
 			$('.sidebar-content').css("overflow-y", "scroll");
-			//$(".sidebar-content").css("top", "0px");
+			$(".sidebar-content").css("top", "");
 		} else {
-			//$(".sidebar-content").css("max-height", "");
+			$(".sidebar-content").css("max-height", "");
 			$(".sidebar-content").css("overflow-y", "");
 		}
-		
-		
+
 		if(bottom_of_sidebar < sticking_point) {
 			if (offset > 219) {
 				$('.sidebar-content').addClass('pinned');
@@ -54,12 +83,11 @@ $(document).ready(function() {
 			$(".sidebar-content").addClass("stuck");
 			$(".sidebar-content").css("top", sticking_point - sidebar_height - $(".sidebar-nav").offset().top);
 		}
-		
 	}
 	
 	$(function() {
 		$(window).scroll(pin_sidebar);
 		$(window).resize(pin_sidebar);
+		$('.sidebar-content').scroll(function(e) { e.stopPropagation(); } );
 	});
-
 });
