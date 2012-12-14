4643dba2aa8758db2f18f1c1f13d1527be9f10df
Dylan Staley <staley.dylan@gmail.com>
2012-12-13 21:49:38 -0600

    Rewrote language-switcher to not be as asinine

--- js/language_switcher.js
+++ js/language_switcher.js
@@ -1,23 +1,10 @@
 function setLanguage(target_language){
-    console.log("Going to remove all languages except "+target_language+".");
-    var index = languages.indexOf(target_language);
-    console.log(target_language+" is at index "+index+".");
-    var languages_to_remove = languages.slice(0);
-    languages_to_remove.splice(index,1);
-    console.log("Going to remove: ");
-    console.log(languages_to_remove);
-    languages_to_remove.forEach(removeLanguage);
+    $('.language').hide();
     if (target_language==".Net") {target_language="dotnet";};
     window.current_language = target_language.toLowerCase();
+    $('.language.'+window.current_language).show();
     $('.dropdown-toggle').html(target_language+' <b class="caret"></b>')
     $('#dropdown-'+window.current_language).addClass('active');
-}
-
-function removeLanguage(value, index, array){
-    value = value.toLowerCase();
-    if (value==".net") {value="dotnet";};
-    $('.'+value).hide();
-    console.log(value);
 }
 
 function switchLanguage(target_language){
@@ -25,8 +12,8 @@ function switchLanguage(target_language){
     $('.dropdown-toggle').html(target_language+' <b class="caret"></b>')
     target_language = target_language.toLowerCase();
     if (target_language==".net") {target_language="dotnet";};
-    $('.'+window.current_language).hide();
-    $('.'+target_language).show();
+    $('.language.'+window.current_language).hide();
+    $('.language.'+target_language).show();
     $('#dropdown-'+window.current_language).removeClass('active');
     $('#dropdown-'+target_language).addClass('active');
     window.current_language = target_language;
