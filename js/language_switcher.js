333f616f70d1bcb3d8a651f649bf2cde856dbdda
Paddy Foran <paddy@iron.io>
2012-12-13 23:27:39 -0500

    Made the language switcher a lot less brittle.

--- js/language_switcher.js
+++ js/language_switcher.js
@@ -1,42 +1,29 @@
-function setLanguage(target_language){
-    console.log("Going to remove all languages except "+target_language+".");
-    var index = languages.indexOf(target_language);
-    console.log(target_language+" is at index "+index+".");
-    var languages_to_remove = languages.slice(0);
-    languages_to_remove.splice(index,1);
-    console.log("Going to remove: ");
-    console.log(languages_to_remove);
-    languages_to_remove.forEach(removeLanguage);
-    if (target_language==".Net") {target_language="dotnet";};
-    window.current_language = target_language.toLowerCase();
-    $('.dropdown-toggle').html(target_language+' <b class="caret"></b>')
-    $('#dropdown-'+window.current_language).addClass('active');
-}
-
-function removeLanguage(value, index, array){
-    value = value.toLowerCase();
-    if (value==".net") {value="dotnet";};
-    $('.'+value).hide();
-    console.log(value);
-}
-
 function switchLanguage(target_language){
-    cacheLanguage(target_language);
-    $('.dropdown-toggle').html(target_language+' <b class="caret"></b>')
-    target_language = target_language.toLowerCase();
-    if (target_language==".net") {target_language="dotnet";};
-    $('.'+window.current_language).hide();
-    $('.'+target_language).show();
-    $('#dropdown-'+window.current_language).removeClass('active');
+    if(!validLanguage(target_language)) {
+        console.log('"'+target_language+'" not found in languages.');
+        return;
+    }
+    cache = true;
+    if(arguments.length == 2) {
+        cache = arguments[1];
+    }
+    $('.dropdown-toggle').html(languages[target_language]+' <b class="caret"></b>')
+    $('.language').hide();
+    $('.language.'+target_language).show();
+    $('#menu3 > .active').removeClass('active');
     $('#dropdown-'+target_language).addClass('active');
-    window.current_language = target_language;
-    cacheLanguage(window.current_language);
+    if(cache) {
+        cacheLanguage(target_language);
+    }
 }
 
 function cacheLanguage(target_language){
-    target_language = $('#dropdown-'+target_language).text();
     if (Modernizr.localstorage) {
         localStorage.setItem("desired_language", target_language);
     }
     Cookies.set("desired_language", target_language);
+}
+
+function validLanguage(target_language) {
+    return target_language in languages;
 }
