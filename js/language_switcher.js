e435d055b923ec33c2dc6252ab7f5a066b19e221
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-08-29 18:08:01 -0700

    Changes to the Language specific dropdown with add UI cues

--- js/language_switcher.js
+++ js/language_switcher.js
@@ -1,3 +1,7 @@
+function validLanguage(target_language) {
+    return target_language in languages;
+}
+
 function switchLanguage(target_language){
     if(!validLanguage(target_language)) {
         console.log('"'+target_language+'" not found in languages.');
@@ -7,11 +11,12 @@ function switchLanguage(target_language){
     if(arguments.length == 2) {
         cache = arguments[1];
     }
-    $('.dropdown-toggle').html(languages[target_language]+' <b class="caret"></b>')
+    $('.dropdown-toggle').html('Documentation for ' + languages[target_language]+' <b class="caret"></b>')
     $('.language').hide();
     $('.language.'+target_language).show();
     $('#menu3 > .active').removeClass('active');
     $('#dropdown-'+target_language).addClass('active');
+    $('.content h1').html("IronMQ " + capitalizeLanguage(target_language) + " Documentation")
     if(cache) {
         cacheLanguage(target_language);
     }
@@ -24,6 +29,18 @@ function cacheLanguage(target_language){
     Cookies.set("desired_language", target_language);
 }
 
-function validLanguage(target_language) {
-    return target_language in languages;
+function capitalizeLanguage(string)
+{   if (string == "php"){
+    return string.toUpperCase(); }
+
+    else if(string == "dotnet"){
+        return ".NET";
+    }
+    else if(string == "Node.js"){
+        return "Node.js";
+    }
+    else {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+
+    }
 }
